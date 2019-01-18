8f8bf09a99f202ca9b7f5fe25c04c40fd1e92178
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-18 12:06:07 +0200

    Initial commit for Gatsby version of dev.hsl.fi
    * Created new site with gatsby-starter-hello-world

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `HSL Developer Community`,
+    colors: {
+      //https://www.hsl.fi/tyyliopas/varit
+      primary: `#007AC9`,
+      secondary: `#BEE4F8`,
+    },
+  },
+}
