db0a7fc1bf37e8fe00f7173b8d486cc35cce8026
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-18 13:56:26 +0200

    Remove text decorations from links

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -7,6 +7,11 @@ const typography = new Typography({
   headerGray: 15,
   bodyFontFamily: ['Georgia', 'serif'],
   bodyGray: 15,
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
+    'a:link': {
+      textDecoration: 'none',
+    },
+  }),
 })
 
 export default typography
