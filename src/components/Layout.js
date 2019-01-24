3a6d472bab0f27ae1bd6d4f35e62fc4cd6b88f94
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-24 10:49:50 +0200

    Set site background with react-helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,36 @@
 import React from 'react'
 
+import { Helmet } from "react-helmet";
+
 import Header from './Header'
 
-import { rhythm } from '../utils/typography'
+import typography from '../utils/typography'
+import { StaticQuery } from 'gatsby';
 
+const { rhythm } = typography;
 
 export default ({children}) => (
-  <>
-    <Header/>
-    { children }
-  </>
+  <StaticQuery query={graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          colors {
+            primary
+            secondary
+          }
+        }
+      }
+    }`
+ } render={ data => (
+    <>
+      <Helmet>
+        <style>{ `html { background-color: ${data.site.siteMetadata.colors.secondary}}` }</style>
+      </Helmet>
+      <Header/>
+      <div style={{ margin: `0 auto`, maxWidth: 1000, padding: `0 ${rhythm(1)}` }}>
+        {children}
+      </div>
+    </>
+ )}></StaticQuery>
 )
