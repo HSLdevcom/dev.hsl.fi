43ea938b340cc35c2fed6f8789bd7c0045a3f4f2
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-25 15:25:19 +0200

    Update Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,18 +32,26 @@ export default () => (
       }`
    } render={data => (
        <div style={{ margin: `0 auto` }}>
-         <header style={{marginBottom: rhythm(2/3), padding: rhythm(1), display: `flex`, justifyContent: `center`, alignItems: `center`, background: data.site.siteMetadata.colors.primary }}>   
-           <a href="https://www.hsl.fi/en">
-              <Img fixed={data.file.childImageSharp.fixed} alt="HSL logo" critical={true} fadeIn={false} style={{marginRight: rhythm(1)}} />
-           </a>  
-           <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, marginRight: `auto` }}>    
-              <h3 style={{ display: `inline`, color: `white` }}>{ data.site.siteMetadata.title }</h3>      
-           </Link>
-           <nav style={{ display: `inline-block` }}>
-             <NavLink to="/projects/">Projects</NavLink>
-             <NavLink to="/apis/">APIs</NavLink>
-             <NavLink to="/contact/">Contact</NavLink>
-           </nav>
+         <header style={{ 
+           marginBottom: rhythm(2/3), 
+           padding: rhythm(1), 
+           display: `flex`, 
+           justifyContent: `start`, 
+           alignItems: `center`, 
+           flexWrap: `wrap`,
+           background: data.site.siteMetadata.colors.primary
+          }}>   
+            <a href="https://www.hsl.fi/en">
+                <Img fixed={data.file.childImageSharp.fixed} alt="HSL logo" critical={true} fadeIn={false} style={{marginRight: rhythm(1)}} />
+            </a>  
+            <Link to="/" style={{ flexGrow: '1', textShadow: `none`, backgroundImage: `none`, marginRight: rhythm(1) }}>    
+                <h3 style={{ display: `inline`, color: `white` }}>{ data.site.siteMetadata.title }</h3>      
+            </Link>
+            <nav style={{ display: `inline-block` }}>
+              <NavLink to="/projects/">Projects</NavLink>
+              <NavLink to="/apis/">APIs</NavLink>
+              <NavLink to="/contact/">Contact</NavLink>
+            </nav>
          </header>
        </div>
      )}
