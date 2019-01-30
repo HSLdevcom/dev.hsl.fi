10bb3fdfb793824d0af5eec6162f91beef79dffd
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-30 15:19:46 +0200

    Add (+ configure) 'gatsby-plugin-offline'

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`
   ]
 };
