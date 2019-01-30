5bac1e46a527870dc295401557ae05361637fdcf
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-30 11:07:21 +0200

    Create 'Content' component

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import typography from '../utils/typography'
+
+const { rhythm } = typography;
+
+export default ({ style, children }) => (
+    <div style={{ background: 'white', borderRadius: rhythm(2/3), padding: rhythm(2/3), marginTop: rhythm(2/3), ...style }}>
+        { children }
+    </div>
+)
