49e34ad4fa8b646fd941e7752bb91a8d6100e8f4
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-30 15:06:12 +0200

    Use Gatsby Remark plugin to generate pages from markdown

---
+++ src/templates/markdown.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
+import Content from "../components/Content";
+
+export default ({ data }) => {
+  const post = data.markdownRemark;
+  return (
+    <>
+      <SEO pageTitle={post.frontmatter.title} pageDescription={post.excerpt} />
+      <Layout>
+        <Content>
+          <h1>{post.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </Content>
+      </Layout>
+    </>
+  );
+};
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+      excerpt(pruneLength: 200, format: PLAIN)
+    }
+  }
+`;
