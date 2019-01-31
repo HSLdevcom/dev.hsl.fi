00a158381cd85bb986faf40a7a8329d0b90f124a
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-01-31 10:06:14 +0200

    Update SEO component

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
 
-const SEO = ({ pageTitle, pageDescription, pageKeywords }) => (
+const SEO = ({ pageTitle, pageDescription, pagePath, pageKeywords }) => (
   <StaticQuery
     query={graphql`
       query {
         site {
           siteMetadata {
+            siteUrl
             title
             keywords
           }
@@ -18,6 +19,9 @@ const SEO = ({ pageTitle, pageDescription, pageKeywords }) => (
       const title = pageTitle
         ? `${pageTitle} | ${metadata.title}`
         : metadata.title;
+
+      const canonicalUrl = metadata.siteUrl + pagePath;
+
       const keywords = [
         ...(metadata.keywords || []),
         ...(pageKeywords || [])
@@ -31,13 +35,17 @@ const SEO = ({ pageTitle, pageDescription, pageKeywords }) => (
             <meta name="description" content={pageDescription} />
           )}
 
+          <link rel="canonical" href={canonicalUrl} />
+
           {keywords.length > 0 && <meta name="keywords" content={keywords} />}
 
           {/* OpenGraph tags http://ogp.me/ */}
+          <meta property="og:type" content="website" />
           <meta property="og:title" content={title} />
           {pageDescription && (
             <meta property="og:description" content={pageDescription} />
           )}
+          <meta property="og:url" content={canonicalUrl} />
 
           {/* Twitter tags https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup.html */}
           <meta property="twitter:title" content={title} />
