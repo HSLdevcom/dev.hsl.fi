4b2d0e39e8fdf81d3834336b55309d6158f76c6f
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-02-08 15:03:45 +0200

    Create component for displaying non-streched images with 'gatsby-image'

---
+++ src/components/NonStrechedImage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Img from "gatsby-image";
+
+const NonStretchedImage = props => {
+  let normalizedProps = props;
+  if (props.fluid && props.fluid.presentationWidth) {
+    normalizedProps = {
+      ...props,
+      style: {
+        ...(props.style || {}),
+        maxWidth: props.fluid.presentationWidth,
+        margin: "0 auto" // Used to center the image
+      }
+    };
+  }
+
+  return <Img {...normalizedProps} />;
+};
+
+export default NonStretchedImage;
