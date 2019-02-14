11a03fa94897ddf06d16b0801f48292be3285cd3
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-02-14 13:27:35 +0200

    Increase minWidth in Responsive component

--- src/components/Responsive.js
+++ src/components/Responsive.js
@@ -2,7 +2,7 @@ import React from "react";
 import Media from "react-media";
 
 export default ({ mobile, desktop }) => (
-  <Media query="(min-width: 600px)" defaultMatches={true}>
+  <Media query="(min-width: 768px)" defaultMatches={true}>
     {matches => (matches ? desktop : mobile)}
   </Media>
 );
