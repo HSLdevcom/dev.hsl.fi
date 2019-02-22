c3044b8de981a0e94a685612e59073d4bfb6791b
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-02-22 11:00:13 +0200

    Add and configure 'gatsby-sitemap-plugin'

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`
   ]
 };
