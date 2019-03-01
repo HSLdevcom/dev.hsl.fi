a9611e2d4b3312116fb14398479985bee63256e9
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-03-01 14:00:51 +0200

    Rewrite Facebook components

--- src/components/Facebook/FacebookTimeline.js
+++ src/components/Facebook/FacebookTimeline.js
@@ -1,22 +1,60 @@
 import React from "react";
 
-export default props => (
-  <div
-    className="fb-page"
-    data-href={`https://www.facebook.com/${props.page}`}
-    data-tabs="timeline"
-    data-height={props.height}
-    data-width={props.width}
-    data-small-header="true"
-    data-adapt-container-width="true"
-    data-hide-cover="false"
-    data-show-facepile="false"
-  >
-    <blockquote
-      cite={`https://www.facebook.com/${props.page}`}
-      className="fb-xfbml-parse-ignore"
-    >
-      <a href={`https://www.facebook.com/${props.page}`}>{props.page}</a>
-    </blockquote>
-  </div>
-);
+import provideFacebook from "./provideFacebook";
+
+class FacebookTimeline extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    this.parseXFBML();
+  }
+
+  componentDidUpdate() {
+    this.parseXFBML();
+  }
+
+  parseXFBML = () => {
+    if (this.props.FB && this.ref) {
+      //https://gist.github.com/RopoMen/29516a94e31455afe9b6
+      this.ref.current.classList.remove(
+        "fb_iframe_widget",
+        "fb_iframe_widget_fluid"
+      );
+
+      this.props.FB.XFBML.parse(this.ref.current.parentNode);
+    }
+  };
+
+  render() {
+    return (
+      <div
+        className="fb-page"
+        data-href={`https://www.facebook.com/${this.props.page}`}
+        data-tabs="timeline"
+        data-height={this.props.height}
+        data-width={this.props.width}
+        data-small-header="true"
+        data-adapt-container-width="true"
+        data-hide-cover="false"
+        data-show-facepile="false"
+        style={{ width: "100%", height: "100%" }}
+        ref={this.ref}
+      >
+        <blockquote
+          cite={`https://www.facebook.com/${this.props.page}`}
+          className="fb-xfbml-parse-ignore"
+        >
+          <a href={`https://www.facebook.com/${this.props.page}`}>
+            {this.props.page}
+          </a>
+        </blockquote>
+      </div>
+    );
+  }
+}
+
+export default provideFacebook(FacebookTimeline);
---
+++ src/components/Facebook/provideFacebook.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const provideFB = WrappedComponent => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { FB: undefined };
+    }
+
+    componentDidMount() {
+      new Promise(resolve => {
+        if (window.FB) {
+          resolve(window.FB);
+          return;
+        }
+
+        window.fbAsyncInit = () => {
+          window.FB.init({
+            xfbml: false,
+            version: "v3.2"
+          });
+
+          resolve(window.FB);
+        };
+
+        if (window.document.getElementById("facebook-jssdk")) {
+          return;
+        }
+
+        const fjs = window.document.getElementsByTagName("script")[0];
+        const js = window.document.createElement("script");
+        js.id = "facebook-jssdk";
+        js.src =
+          "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2";
+
+        fjs.parentNode.insertBefore(js, fjs);
+      }).then(FB => this.setState({ FB }));
+    }
+
+    render() {
+      if (!this.state.FB) {
+        return null;
+      }
+
+      return <WrappedComponent FB={this.state.FB} {...this.props} />;
+    }
+  };
+};
+
+export default provideFB;
