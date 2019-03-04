5d3c61833c733f5da68fd85f9505227d09b5e931
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-03-04 15:21:09 +0200

    Add state to 'Responsive' component to render correct component initially (ReactTraining/react-media#91)

--- src/components/Responsive.js
+++ src/components/Responsive.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Media from "react-media";
 
-export default ({ mobile, desktop }) => (
-  <Media query="(min-width: 768px)" defaultMatches={true}>
-    {matches => (matches ? desktop : mobile)}
-  </Media>
-);
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { client: 0 };
+  }
+
+  componentDidMount() {
+    this.setState({ client: 1 });
+  }
+
+  render() {
+    return (
+      <Media
+        key={this.state.client}
+        query="(min-width: 768px)"
+        defaultMatches={true}
+      >
+        {matches => (matches ? this.props.desktop : this.props.mobile)}
+      </Media>
+    );
+  }
+}
