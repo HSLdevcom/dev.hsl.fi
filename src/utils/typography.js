ed05818e51a18cbfdac24b8acdadf100e58319a5
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-03-14 12:54:40 +0200

    Change to OpenSans font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,17 @@
 import Typography from "typography";
 
 const typography = new Typography({
+  googleFonts: [
+    {
+      name: "Open Sans",
+      styles: ["400", "700"]
+    }
+  ],
   baseFontSize: "16px",
   baseLineHeight: 1.5,
-  headerFontFamily: ["Montserrat", "sans-serif"],
+  headerFontFamily: ["Open Sans", "sans-serif"],
   headerGray: 15,
-  bodyFontFamily: ["Georgia", "serif"],
+  bodyFontFamily: ["Open Sans", "sans-serif"],
   bodyGray: 15,
   overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
     "a:link": {
