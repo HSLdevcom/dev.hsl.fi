e5f5c42a7f4147e9c8688c4d2f489e1b66cca24c
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-03-20 15:33:22 +0200

    Add + configure 'gatsby-remark-images' plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,22 @@ module.exports = {
         path: path.join(__dirname, `src`, `content`)
       }
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1000
+            }
+          }
+        ]
+      }
+    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
